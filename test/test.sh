e1b0cdceaba71e331cec84b94a11d0c3a12778f2
itsukikawamu <itsuki0204@gmail.com>
2026-02-03 21:44:24 +0900

    add test

--- test/test.sh
+++ test/test.sh
@@ -5,10 +5,10 @@ assert()
 	expected="$1"
 	input="$2"
 	src=test.txt
-	exe="c.out"
+	exe="b.out"
 
 	echo "$2" > "$src"
-	./Ccc "$src"
+	./ccc "$src"
 	./"$exe"
 	actual="$?"
 	if [ "$actual" == "$expected" ]; then
@@ -25,12 +25,18 @@ make re > /dev/null
 
 assert 0 0
 assert 42 42
-assert 21 "5+20-4"
+assert 21 " 5 +20-4 "
 assert 9 "1+0+10-2"
 assert 9 "1 + 2 - 3 +  4  +  5"
 assert 2 "1 + 3 - 2"
 assert 32 "  3 * 6+2 * 7  "
 assert 6 "4 * (2 + 1) / ((1 * 1) + 1)"
 assert 6 "-3 *-3 + (-6 / (1 + 1))"
+assert 1 "10 == 10 "
+assert 0 "-10 != (-10)"
+assert 1 "6 >= 6"
+assert 0 "6 < 6"
+assert 1 "(1 + 3) ==4"
+assert 1 "(((( 1 + 1 ) ) )  )  < (2 * 2 / 2 * 2)"
 
 echo OK
