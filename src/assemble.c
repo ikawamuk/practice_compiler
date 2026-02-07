ef4c7264575183ca27527141863b9143e4c24471
ikawamuk <ikawamuk@student.42.fr>
2026-02-07 15:18:06 +0900

    update: assemble must be gcc

--- src/assemble.c
+++ src/assemble.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 21:39:32 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/02/03 18:41:23 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/02/07 15:15:02 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	assemble(char *asm_file_name)
 {
 	char	cmd[256];
 
-	snprintf(cmd, sizeof(cmd), "cc %s -o b.out", asm_file_name);
+	snprintf(cmd, sizeof(cmd), "gcc %s -o b.out", asm_file_name);
 	if (system(cmd) != 0)
 		return (EXIT_FAILURE);
 	// snprintf(cmd, sizeof(cmd), "rm %s", asm_file_name);
