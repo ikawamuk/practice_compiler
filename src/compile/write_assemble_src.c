ade4c55257600977ac79f404b12f91b080a12611
ikawamuk <ikawamuk@student.42.fr>
2026-02-15 02:57:35 +0900

    update: add function fotter

--- src/compile/write_assemble_src.c
+++ src/compile/write_assemble_src.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 21:39:26 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/02/14 23:00:48 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/02/15 02:55:23 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@
 #include <unistd.h>
 
 static void	write_hedder(FILE *assem_src);
-static void	write_footer(FILE *assem_src);
 static void	write_body(FILE *assem_src, t_program *prog);
 static void	generate_functinon_header(FILE *assem_src, t_program *prog);
+static void	write_functinon_footer(FILE *assem_src);
 void		generate(FILE *assem_src, const t_tree *ast);
 
 void	write_assemble_src(FILE *assem_src, t_program *prog)
 {
 	write_hedder(assem_src);
 	write_body(assem_src, prog);
-	write_footer(assem_src);
 	return ;
 }
 
@@ -42,6 +41,14 @@ static void	write_body(FILE *assem_src, t_program *prog)
 		generate(assem_src, ast);
 		ast = next;
 	}
+	write_functinon_footer(assem_src);
+}
+
+static void	write_hedder(FILE *assem_src)
+{
+	fprintf(assem_src, ".intel_syntax noprefix\n.globl main\n");
+	fprintf(assem_src, "main:\n");
+	return ;
 }
 
 static void	generate_functinon_header(FILE *assem_src, t_program *prog)
@@ -52,14 +59,7 @@ static void	generate_functinon_header(FILE *assem_src, t_program *prog)
 		fprintf(assem_src, "\tsub rsp, %d\n", prog->var_list->offset);
 }
 
-static void	write_hedder(FILE *assem_src)
-{
-	fprintf(assem_src, ".intel_syntax noprefix\n.globl main\n");
-	fprintf(assem_src, "main:\n");
-	return ;
-}
-
-static void	write_footer(FILE *assem_src)
+static void	write_functinon_footer(FILE *assem_src)
 {
 	fprintf(assem_src, "\tmov rsp, rbp\n");
 	fprintf(assem_src, "\tpop rbp\n");
