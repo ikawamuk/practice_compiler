45d3a53a4f166da7da3ec4ce19531715b0a2856e
ikawamuk <ikawamuk@student.42.fr>
2026-02-15 21:54:06 +0900

    update: test ok

--- test/test.sh
+++ test/test.sh
@@ -48,5 +48,6 @@ assert 3 "{if (1-1) return 2; return 3;}"
 assert 2 "{if (1) return 2; return 3;}"
 assert 2 "{if (2 - 1) return 2; return 3;}"
 assert 10 "{i = 0; while (i < 10) i= i + 1; return i;}"
+assert 10 "{print_ok(); return 10;}"
 
 echo OK
